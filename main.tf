74d16c80ec91aa8afc25af44087bebc2e4bb002a
Alvaro Stivi <astivi@google.com>
2021-05-24 22:38:41 +0000

    Adds frontend build to Terraform script
    Change-Id: Ifa3e68929a3d6bdb5452bf3800b16b58d54589af

--- main.tf
+++ main.tf
@@ -12,51 +12,86 @@ resource "random_string" "random" {
 }
 
 locals {
-    new_project_id = "goop-4-all-${random_string.random.result}"
+    new_project_id = "co-op-4-all-${random_string.random.result}"
 }
 
-# Creation of new project to host Goop
+resource "null_resource" "ng_cli" {
+  provisioner "local-exec" {
+    working_dir = "frontend"
+    command = "sudo npm install -g @angular/cli"
+  }
+}
 
-resource "google_project" "goop_project" {
-  name       = "Goop 4 All"
+resource "null_resource" "ng_dependencies" {
+  provisioner "local-exec" {
+    working_dir = "frontend"
+    command = "npm install"
+  }
+}
+
+resource "null_resource" "angular_build" {
+  provisioner "local-exec" {
+    working_dir = "frontend"
+    command = "ng build --configuration=production"
+  }
+
+  depends_on = [null_resource.ng_cli, null_resource.ng_dependencies]
+}
+
+resource "null_resource" "zip_dist" {
+  provisioner "local-exec" {
+    command = "zip co-op-4-all.zip app.yaml requirements.txt frontend/dist/*"
+  }
+
+  depends_on = [null_resource.angular_build]
+}
+
+# Creation of new project to host Co-op 4 All
+
+resource "google_project" "co_op_4_all_project" {
+  name       = "Co-op 4 All"
   project_id = local.new_project_id
   folder_id = var.project_folder_id
   billing_account = var.billing_account
+  depends_on = [null_resource.angular_build]
 }
 
-# Uploads Goop package to GCS
-resource "google_storage_bucket" "goop_bucket" {
+# Uploads Co-op 4 all package to GCS
+resource "google_storage_bucket" "co_op_4_all_bucket" {
   name = "${local.new_project_id}-appengine"
   project = local.new_project_id
   uniform_bucket_level_access = true
-  depends_on = [google_project.goop_project]
+  depends_on = [google_project.co_op_4_all_project, null_resource.zip_dist]
 }
 
-resource "google_storage_bucket_object" "goop_package" {
-  name   = "goop.zip"
-  bucket = google_storage_bucket.goop_bucket.name
-  source = "./packages/goop.zip"
-  depends_on = [google_storage_bucket.goop_bucket]
+resource "google_storage_bucket_object" "co_op_4_all_package" {
+  name   = "co-op-4-all.zip"
+  bucket = google_storage_bucket.co_op_4_all_bucket.name
+  source = "./co-op-4-all.zip"
+  depends_on = [google_storage_bucket.co_op_4_all_bucket]
 }
-
-#resource "google_service_account" "cloud_build_service_account" {
-#  account_id   = "${google_project.goop_project.number}@cloudbuild.gserviceaccount.com"
-#  display_name = "Service Account"
-#}
 
 resource "google_project_service" "cloudbuild_api" {
   project                    = local.new_project_id
   service                    = "cloudbuild.googleapis.com"
   disable_dependent_services = false
   disable_on_destroy         = false
-  depends_on = [google_storage_bucket.goop_bucket]
+  depends_on = [google_storage_bucket.co_op_4_all_bucket]
+}
+
+resource "google_project_service" "campain_manager_api" {
+  project                    = local.new_project_id
+  service                    = "dfareporting.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+  depends_on = [google_project.co_op_4_all_project]
 }
 
 resource "google_storage_bucket_iam_member" "member" {
-  bucket = google_storage_bucket.goop_bucket.name
+  bucket = google_storage_bucket.co_op_4_all_bucket.name
   role = "roles/storage.admin"
-  member = "serviceAccount:${google_project.goop_project.number}@cloudbuild.gserviceaccount.com"
-  depends_on = [google_app_engine_application.app, google_storage_bucket.goop_bucket, google_project_service.cloudbuild_api]
+  member = "serviceAccount:${google_project.co_op_4_all_project.number}@cloudbuild.gserviceaccount.com"
+  depends_on = [google_app_engine_application.app, google_storage_bucket.co_op_4_all_bucket, google_project_service.cloudbuild_api]
 }
 
 # Creates App Engine 
@@ -64,10 +99,10 @@ resource "google_storage_bucket_iam_member" "member" {
 resource "google_app_engine_application" "app" {
   project     = local.new_project_id
   location_id = var.location
-  depends_on = [google_project.goop_project]
+  depends_on = [google_project.co_op_4_all_project]
 }
 
-resource "google_app_engine_standard_app_version" "goop_application" {
+resource "google_app_engine_standard_app_version" "co_op_4_all_application" {
   version_id = "v1"
   service    = "default"
   runtime    = "python39"
@@ -79,7 +114,7 @@ resource "google_app_engine_standard_app_version" "goop_application" {
 
   deployment {
     zip {
-      source_url = "https://storage.googleapis.com/${google_storage_bucket.goop_bucket.name}/${google_storage_bucket_object.goop_package.name}"
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.co_op_4_all_bucket.name}/${google_storage_bucket_object.co_op_4_all_package.name}"
     }
   }
 
@@ -102,5 +137,5 @@ resource "google_app_engine_standard_app_version" "goop_application" {
   }
   noop_on_destroy = true
 
-  depends_on = [google_app_engine_application.app, google_storage_bucket_object.goop_package, google_storage_bucket_iam_member.member]
+  depends_on = [google_app_engine_application.app, google_storage_bucket_object.co_op_4_all_package, google_storage_bucket_iam_member.member]
 }
