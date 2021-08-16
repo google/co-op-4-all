4c8d4a31f11055899a64f60667bf12c5a8c9a89d
Ana Esqueda <anaesqueda@google.com>
2021-08-16 19:34:41 +0000

    Add missing license to all the files in the frontend.

--- frontend/src/app/modules/co-op-configurations/co-op-configurations.component.spec.ts
+++ frontend/src/app/modules/co-op-configurations/co-op-configurations.component.spec.ts
@@ -1,20 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/***************************************************************************
+*
+*  Copyright 2021 Google Inc.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*
+*  Note that these code samples being shared are not official Google
+*  products and are not formally supported.
+*
+***************************************************************************/
 
-import { CoOpConfigurationsComponent } from './co-op-configurations.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CoopConfigurationsComponent } from './co-op-configurations.component';
 
 describe('CoOpConfigurationsComponent', () => {
-  let component: CoOpConfigurationsComponent;
-  let fixture: ComponentFixture<CoOpConfigurationsComponent>;
+  let component: CoopConfigurationsComponent;
+  let fixture: ComponentFixture<CoopConfigurationsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CoOpConfigurationsComponent ]
+      declarations: [ CoopConfigurationsComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CoOpConfigurationsComponent);
+    fixture = TestBed.createComponent(CoopConfigurationsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
